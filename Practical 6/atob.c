58939640b8ce1e8fd3f629d0781ed0785d89a320
Aanchal Dhangar <aanchaldhangar22@gmail.com>
2025-12-23 15:07:05 +0530

    Practical 6 edit

---
+++ Practical 6/atob.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+printf (" Aanchal Dhangar RU2510010\n");
+int A,B;
+scanf ( "%d %d", &A , &B);
+for ( int i = A; i <= B; i++){
+printf ("%d ",i);
+}
+return 0;
+}
+//output
+//  Aanchal Dhangar RU2510010
+// 6 8
+// 6 7 8 
